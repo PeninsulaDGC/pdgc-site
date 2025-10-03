c3e5e368f33b207adc8099ad896202d8e301f725
PeninsulaDGC <peninsuladiscgolfclub@gmail.com>
2025-10-03 15:42:29 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pdgc-site"
+  base: "/pdgc-site",
   plugins: [react()],
   // This is important for GitHub Pages so asset paths resolve correctly
   base: './',
